13018f419330bedeb9129baba552cfdb5ec2577f
Ramon Santos <ramonss.bque@gmail.com>
2024-11-13 21:40:30 -0300

    .gitignore

--- Program.cs
+++ Program.cs
@@ -4,15 +4,14 @@ using Microsoft.Extensions.DependencyInjection;
 
 var services = new ServiceCollection();
 services.AddMediatR(typeof(Program));
-var provider = services.BuildServiceProvider();
-var mediator = provider.GetRequiredService<IMediator>();
+// var provider = services.BuildServiceProvider();
+// var mediator = provider.GetRequiredService<IMediator>();
 
-// Executa um comando para adicionar números
-await mediator.Send(new AddNumberCommand(10));
-await mediator.Send(new AddNumberCommand(20));
+// await mediator.Send(new AddNumberCommand(10));
+// await mediator.Send(new AddNumberCommand(20));
 
-// Executa uma consulta para obter a soma
-var result = await mediator.Send(new GetSumQuery());
-Console.WriteLine($"A soma dos números é: {result}");
+// // Executa uma consulta para obter a soma
+// var result = await mediator.Send(new GetSumQuery());
+// Console.WriteLine($"A soma dos números é: {result}");
 
 
